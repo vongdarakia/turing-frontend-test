24b7a9caf3aa8883e688e0a2e2b89d07240b595c
Akia Vongdara <vongdarakia@gmail.com>
2019-04-24 06:11:41 -0700

    Updates

---
+++ precache-manifest.0765509e27a68518a179ded8227be529.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a36f0adf6edd3e51c70",
+    "url": "/turing-frontend-test/static/js/runtime~main.0bb42227.js"
+  },
+  {
+    "revision": "68a0ed4e144d53a140c1",
+    "url": "/turing-frontend-test/static/js/main.33ad8ca2.chunk.js"
+  },
+  {
+    "revision": "6204f67d7bd2227943e5",
+    "url": "/turing-frontend-test/static/js/2.307fe843.chunk.js"
+  },
+  {
+    "revision": "68a0ed4e144d53a140c1",
+    "url": "/turing-frontend-test/static/css/main.234fb893.chunk.css"
+  },
+  {
+    "revision": "79fdee80b3683f2df86bc39edfa923a1",
+    "url": "/turing-frontend-test/index.html"
+  }
+];
